e2e07511f99939958c2ca5bf6064c5b229bd4b70
thanh-do28 <96042778+thanh-do28@users.noreply.github.com>
2023-06-05 12:14:31 +0700

    first commit

--- includes/content-todolist.php
+++ includes/content-todolist.php
@@ -1,5 +1,42 @@
+<?php
+
+
+global $wpdb;
+$result = $wpdb->get_results('SELECT * FROM wp_todos WHERE user_id = 15 ORDER BY date_time DESC');
+
+
+$todolist = [];
+$datelist = [];
+foreach ($result as $todo) {
+    $data = json_encode((array)$todo);
+    array_push($todolist, $data);
+    // echo $data;
+    array_push($datelist, date("Y-m-d", strtotime($todo->date_time)));
+};
+
+$dates = array_unique($datelist);
+// print_r($todolist);
+// print_r($dates);
+?>
+<?php if (empty($result)) { ?>
+    <div class="todoList">
+        <img src="<?php echo get_template_directory_uri(); ?>../images/to-do-7214069__340.webp" alt="" />
+        <h2>Add your first todo</h2>
+        <p>What do you want to get done today?</p>
+    </div>
+    <?php } else {
+    if (strlen($data) <= 0) { ?>
+        <div class="todoList">
+            <img src="<?php echo get_template_directory_uri(); ?>../images/to-do-7214069__340.webp" alt="" />
+            <h2>Add your first todo</h2>
+            <p>What do you want to get done today?</p>
+        </div>
+<?php }
+} ?>
+
 <div class="date">
     <h6 class="pl-3 text-primary">12/02/2023</h6>
+
     <div class="todo-item border border-1 mb-2 px-3 rounded">
         <button class="d-block float-right border rounded mt-1 ">x</button>
         <input type="checkbox" class="check-box" checked />
--- includes/form-todolist.php
+++ includes/form-todolist.php
@@ -1,4 +1,4 @@
-<form id="presently" class='d-flex flex-column' action="" method="" autocomplete="off">
+<form id="presently" class='d-flex flex-column' action="http://localhost/wordpress/wp-content/plugins/insert-todo/simple-contact-todo.php" method="post" autocomplete="off">
     <label>
         <input class="w-100 p-1" style="outline: none;" type="text" placeholder="What do you need to do?" name="title"></input>
     </label>
