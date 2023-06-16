d880c31bb8b527bb2fff5ebca2fc565704919e7d
thanh-do28 <96042778+thanh-do28@users.noreply.github.com>
2023-06-16 18:42:07 +0700

    first commit

--- header.php
+++ header.php
@@ -16,13 +16,42 @@
 
     <header>
         <div class="container">
+            <div class="menu-top-bas-container">
+                <ul id="menu-top-bas" class="top-bar">
+                    <li id="menu-item-18" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-18"><a href="http://localhost/wordpress/">Home</a></li>
+                    <li id="menu-item-19" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-19"><a href="http://localhost/wordpress/about-us/">About Us</a></li>
+
+                    <?php
+                    if (isset($_COOKIE["user_login_id"])) {
+                    ?>
+
+                        <li id="menu-item-21" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-15 current_page_item menu-item-20">
+                            <div class="user-img">
+                                <span class="img">
+                                    <img src="<?php echo get_template_directory_uri(); ?>./images/none-avatar.png" alt="#">
+                                </span>
+                                <span class="name"><?php echo $_COOKIE['user_login_name']; ?></span>
+                            </div>
+                        </li>
+
+
+                    <?php } else { ?>
+                        <li id="menu-item-20" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-15 current_page_item menu-item-20"><a href="http://localhost/wordpress/login/" aria-current="page">Login</a></li>
+                        <li id="menu-item-45" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-45"><a href="http://localhost/wordpress/register/">Register</a></li>
+                    <?php } ?>
+
+
+
+                </ul>
+            </div>
+
             <?php
-            wp_nav_menu(
-                array(
-                    'theme_location' => 'top-menu',
-                    'menu_class' => 'top-bar',
-                )
-            );
+            // wp_nav_menu(
+            //     array(
+            //         'theme_location' => 'top-menu',
+            //         'menu_class' => 'top-bar',
+            //     )
+            // );
             ?>
         </div>
 
